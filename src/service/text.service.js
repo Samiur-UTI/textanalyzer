0b9235a36f195617204c095b1c0499c666913640
samiur <samiur@synesisit.info>
2024-02-12 19:05:03 +0600

    feat(longest-word-in-paragraph):longest word in paragraph done

--- src/service/text.service.js
+++ src/service/text.service.js
@@ -4,10 +4,12 @@ const path = require("path");
 const defaultTextFilePath = path.join(__dirname, "../../storage/");
 
 const wordCount = async (fileId) => {
-  const data = fileId ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8") : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8")
-  const sanitizedData = data.replace(/"/g, '')
+  const data = fileId
+    ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8")
+    : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
+  const sanitizedData = data.replace(/"/g, "");
   const totalWordsCount = sanitizedData.split(" ").length;
-  const uniqueWords =new Set(sanitizedData.toLowerCase().split(" "))
+  const uniqueWords = new Set(sanitizedData.toLowerCase().split(" "));
   return {
     numberOfWords: totalWordsCount,
     numberOfUniqueWords: uniqueWords.size,
@@ -15,58 +17,81 @@ const wordCount = async (fileId) => {
 };
 
 const characterCount = async (fileId) => {
-    const data = fileId ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8") : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
-    const sanitizedData = data.replace(/[.".]/g, '')
-    const totalWords = sanitizedData.split(" ");
-    let charLength=0;
-    for (let i=0;i<totalWords.length;i++){
-        charLength+=totalWords[i].length
-    }
-    // Finding all distinct chars in O(n) time and constant space
-    const totalUniqueChars = findDistinctCharacters(sanitizedData);
-    return {
-        charLength,
-        totalUniqueChars
-    }
+  const data = fileId
+    ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8")
+    : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
+  const sanitizedData = data.replace(/[.".]/g, "");
+  const totalWords = sanitizedData.split(" ");
+  let charLength = 0;
+  for (let i = 0; i < totalWords.length; i++) {
+    charLength += totalWords[i].length;
+  }
+  // Finding all distinct chars in O(n) time and constant space
+  const totalUniqueChars = findDistinctCharacters(sanitizedData);
+  return {
+    charLength,
+    totalUniqueChars,
+  };
 };
 
 function findDistinctCharacters(s) {
-    if (!s) {
-        return "";
+  if (!s) {
+    return "";
+  }
+  // Initialize a bitmask to keep track of encountered characters
+  let bitmask = 0;
+  // Initialize a result string
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    let charValue = s.charCodeAt(i);
+    if ((bitmask & (1 << charValue)) === 0) {
+      result += s[i];
+      bitmask |= 1 << charValue;
     }
-    // Initialize a bitmask to keep track of encountered characters
-    let bitmask = 0;
-    // Initialize a result string
-    let result = "";
-    for (let i = 0; i < s.length; i++) {
-        let charValue = s.charCodeAt(i);
-        if ((bitmask & (1 << charValue)) === 0) {
-            result += s[i];
-            bitmask |= (1 << charValue);
-        }
-    }
-    return result.length-1;
+  }
+  return result.length - 1;
 }
 
 const sentenceCount = async (fileId) => {
-    const data = fileId ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8") : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
-    let sentences=0;
-    for(let i=0;i<data.length;i++){
-        if(data[i] === "."){
-            sentences+=1
-        }
+  const data = fileId
+    ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8")
+    : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
+  let sentences = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === ".") {
+      sentences += 1;
     }
-    return sentences
+  }
+  return sentences;
 };
 
 const paragraphCount = async (fileId) => {
-    const data = fileId ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8") : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
-    return data.split("\n").filter(d => d !== '' && d !== '.').length
+  const data = fileId
+    ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8")
+    : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
+  return data.split("\n").filter((d) => d !== "" && d !== ".").length;
 };
 
 const largestWordsinParagraphs = async (fileId) => {
-    const data = fileId ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8") : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
-    const sanitizedData = data.split("\n").filter(d => d !== '' && d !== '.')
+  const data = fileId
+    ? fs.readFileSync(`${defaultTextFilePath}${fileId}`, "utf-8")
+    : fs.readFileSync(`${defaultTextFilePath}sample.txt`, "utf-8");
+  const sanitizedData = data
+    .split("\n")
+    .filter((d) => d !== "" && d !== ".")
+    .map((str) => str.replace(/[.".]/g, ""))
+    .map((str) => str.split(" "));
+  const largestWords = [];
+  for (let i = 0; i < sanitizedData.length; i++) {
+    let l = 0;
+    for (let j = 0; j < sanitizedData[i].length; j++) {
+      if (sanitizedData[i][j].length > l) {
+        l = sanitizedData[i][j].length;
+      }
+    }
+    largestWords.push(l);
+  }
+  return largestWords;
 };
 
 module.exports = {
