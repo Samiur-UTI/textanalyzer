09fdf3397d45474bde904eb381bc782825c4f9fe
samiur <samiur@synesisit.info>
2024-02-12 09:02:51 +0600

    feat(config):seperated app build from docker and changed port

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Use user routes
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
